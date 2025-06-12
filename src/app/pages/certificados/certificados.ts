2874aa2e11c0c55df34cceba3e6c195c6a721a6e
diegocadilam <diego.rodrigues@cadilam.com.br>
2025-06-12 15:58:18 -0300

    Criado o primeiro estado da página de certificado

---
+++ src/app/pages/certificados/certificados.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SecundaryButton } from "../../_components/secundary-button/secundary-button";
+
+@Component({
+  selector: 'app-certificados',
+  imports: [SecundaryButton],
+  templateUrl: './certificados.html',
+  styleUrl: './certificados.css'
+})
+export class Certificados {
+
+}
