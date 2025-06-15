4e774303a8f945a24ba576efaedb26ef7040950d
diegocadilam <diego.rodrigues@cadilam.com.br>
2025-06-15 17:08:19 -0300

    Desenvolvendo o formulário de atividades

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./_components/navbar/navbar";
 import { CommonModule } from '@angular/common';
+import { PrimaryButton } from "./_components/primary-button/primary-button";
+import { SecundaryButton } from "./_components/secundary-button/secundary-button";
+import { ItemCertificado } from "./_components/item-certificado/item-certificado";
+import { BaseUi } from "./_components/base-ui/base-ui";
+import { Certificados } from './pages/certificados/certificados';
+import { CertificadoForm } from "./pages/certificado-form/certificado-form";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, CommonModule],
+  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecundaryButton, ItemCertificado, BaseUi, Certificados, CertificadoForm],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
