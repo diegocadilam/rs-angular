c3622d713f9261c6aebd104da116d35bc1a62cc7
diegocadilam <diego.rodrigues@cadilam.com.br>
2025-06-16 12:03:01 -0300

    exibindo mensagem de erro simples

--- src/app/pages/certificado-form/certificado-form.ts
+++ src/app/pages/certificado-form/certificado-form.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { SecundaryButton } from "../../_components/secundary-button/secundary-button";
 import { PrimaryButton } from "../../_components/primary-button/primary-button";
+import { FormsModule, NgModel } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-certificado-form',
-  imports: [SecundaryButton, PrimaryButton],
+  imports: [FormsModule, SecundaryButton, PrimaryButton, CommonModule],
   templateUrl: './certificado-form.html',
   styleUrl: './certificado-form.css'
 })
 export class CertificadoForm {
+  nome: string = '';
+  atividade: string = '';
+  atividades: string[] = ['React', 'Angular', 'Java'];
 
+  campoInvalido(control: NgModel){
+    return control.invalid && control.touched;
+  }
 }
--- src/app/pages/certificados/certificados.ts
+++ src/app/pages/certificados/certificados.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 
 import { ItemCertificado } from "../../_components/item-certificado/item-certificado";
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-certificados',
-  imports: [RouterLink, ItemCertificado],
+  imports: [ItemCertificado],
   templateUrl: './certificados.html',
   styleUrl: './certificados.css'
 })
 export class Certificados {
-
 }
